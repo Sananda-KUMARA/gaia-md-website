3bea565880ec8d36369a1b69721787411c64ea6a
Florent LEFEVRE <lefevre.florent@gmail.com>
2025-02-25 05:36:28 +0100

    video fix

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -8,11 +8,13 @@ import videoSrc from '../../videos/video1.mp4'
 export default function VideoPlayer() {
   return (
     <div className="video-container">
-      <Video 
-        src={videoSrc}
-        controls
-        className="w-full h-auto"
-      />
+<video
+  src={videoSrc}
+  controls
+  width={1280}
+  height={720}
+  className="w-full"  // Cette classe permettra à la vidéo d'être responsive
+/>
     </div>
   )
 }
