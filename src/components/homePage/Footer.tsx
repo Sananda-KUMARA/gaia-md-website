c8a9d281bad3bad040a10ee410724a69ac53bf2b
Florent LEFEVRE <lefevre.florent@gmail.com>
2025-02-25 05:45:22 +0100

    fix footer typescript problem

--- src/components/homePage/Footer.tsx
+++ src/components/homePage/Footer.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react';
 
 // Footer navigation data
 const footerNavigation = {
@@ -13,7 +14,7 @@ const footerNavigation = {
     {
       name: 'Facebook',
       href: '#',
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -26,7 +27,7 @@ const footerNavigation = {
     {
       name: 'Instagram',
       href: '#',
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -39,7 +40,7 @@ const footerNavigation = {
     {
       name: 'X',
       href: '#',
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
         </svg>
@@ -48,7 +49,7 @@ const footerNavigation = {
     {
       name: 'GitHub',
       href: '#',
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -61,7 +62,7 @@ const footerNavigation = {
     {
       name: 'YouTube',
       href: '#',
-      icon: (props) => (
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -86,12 +87,15 @@ export default function Footer() {
           ))}
         </nav>
         <div className="mt-16 flex justify-center gap-x-10">
-          {footerNavigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-800">
-              <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" className="size-6" />
-            </a>
-          ))}
+          {footerNavigation.social.map((item) => {
+            const IconComponent = item.icon;
+            return (
+              <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-800">
+                <span className="sr-only">{item.name}</span>
+                <IconComponent aria-hidden="true" className="size-6" />
+              </a>
+            );
+          })}
         </div>
         <p className="mt-10 text-center text-sm/6 text-gray-600">
           &copy; 2025 - Gaïa Motion Design. All rights reserved.
