62154ef25fa773933a71927443226f2c11c822d6
Florent LEFEVRE <lefevre.florent@gmail.com>
2025-03-04 03:52:25 +0100

    import SpeedInsights vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
